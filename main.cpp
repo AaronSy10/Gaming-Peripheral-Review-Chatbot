147919e930dcf98ad2be252a155c4cd150effe21
rave <84365022+ravephelps@users.noreply.github.com>
2022-01-09 03:33:19 +0800

    Sentiment rating system added
    Kulang pa neto:
    - increment value sa database
    - display summary of reviews
    - new product (?)
    - admin access
    - flavor text and conversations

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 #include <mysql.h>
 #include <mysqld_error.h>
 #include <sstream>
+#include <bits/stdc++.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std; 
 
-const char* hostname  = "localhost"; //database credentials
+const char* hostname  = "localhost";
 const char* username  = "root";
-const char* password = "aaronsy10";
+const char* password = "RavidPhelpsR212000";
 const char* database = "sentibot";
 unsigned int port = 3306;
 const char* unixsocket = NULL;
@@ -17,7 +18,7 @@ unsigned long clientflag = 0;
 MYSQL* connectdatabase(){
 	MYSQL*conn;
 	conn = mysql_init(0);
-	conn = mysql_real_connect (conn, hostname, username, password, database, port, unixsocket, clientflag); 
+	conn = mysql_real_connect (conn, hostname, username, password, database, port, unixsocket, clientflag);
 	//connecting to database using credentials
 	if (conn){
 		cout<<"Connected to database"<<endl;
@@ -37,10 +38,10 @@ insert (MYSQL* conn){
 	cout<< "[0]: No" << endl;
 	cout<< "User: ";
 	getline(cin>>ws, decision);
-	if (decision == "Yes"||decision == "yes"||decision == "YES"||decision == "1"){//if admin wants to add
+	if (decision == "Yes"||decision == "yes"||decision == "YES"||decision == "1"){
 		cout<< "Enter admin password: ";
 		getline(cin>>ws, adminPassword);
-		if (adminPassword == "1234"){ //adding for admin only
+		if (adminPassword == "1234"){
 			stringstream ss;
 			string userQuerydb, botResponsedb;
 			cout<<"\nEnter userQuery: ";
@@ -82,16 +83,17 @@ display(MYSQL* conn,string userChat, int adder){
 			res = mysql_store_result (conn);
 			int count = mysql_num_fields(res);
 			while (row = mysql_fetch_row(res)){
-				for(int i=0;i<count;i++){ //look through all the data 
+				for(int i=0;i<count;i++){
 					responseCounter = i;
 				}
 				cout<<"SentiBOT: "<<row[responseCounter];
 				adder=1;
 				cout<<endl;
 			}
-		if (adder==0){ //if data is not found on the database
-			insert(conn);
-		}
+			if (adder==0){
+				insert(conn);
+				adder=1;
+			}
 		}
 	}
 	else{
@@ -99,6 +101,98 @@ display(MYSQL* conn,string userChat, int adder){
 	}
 }
 
+int checkPositive(MYSQL* conn,string userChat){
+	MYSQL_ROW row;
+	MYSQL_RES* res;
+	int result;
+	stringstream ss;
+	ss<<"SELECT COUNT(*) FROM sentibot.positive WHERE positiveword='"+userChat+"'";
+	string query = ss.str();
+	const char* q = query.c_str();
+
+	if (conn){
+		int qstate = mysql_query(conn, q); //executes query
+		res = mysql_store_result (conn);   //stores result 
+		row = mysql_fetch_row(res);		   //gets value of row
+		stringstream toint(row[0]);		   //convert from char* to int
+		toint >> result;				   //puts int in result var
+		return result;					   //returns result of analysis to main
+	}
+	else{
+		cout<<"Failed to fetch";
+	}
+}
+
+
+int checkNegative(MYSQL* conn,string userChat){
+	MYSQL_ROW row;
+	MYSQL_RES* res;
+	int result;
+	stringstream ss;
+	ss<<"SELECT COUNT(*) FROM sentibot.negative WHERE negativeword='"+userChat+"'";
+	string query = ss.str();
+	const char* q = query.c_str();
+
+	if (conn){
+		int qstate = mysql_query(conn, q); //executes query
+		res = mysql_store_result (conn);   //stores result 
+		row = mysql_fetch_row(res);		   //gets value of row
+		stringstream toint(row[0]);		   //convert from char* to int
+		toint >> result;				   //puts int in result var
+		return result;					   //returns result of analysis to main
+	}
+	else{
+		cout<<"Failed to fetch";
+	}
+}
+
+
+int wordCount(string str){
+	stringstream s(str);
+	string word;
+	int count = 0;
+	while (s >> word)
+		count ++;
+	return count;
+}
+
+int cleanStatement(MYSQL* conn, string text){   //to use function cleanStatement(connection, string to be cleaned)
+	
+	//remove punctuations
+	for (int i = 0, len = text.size(); i < len; i++)
+    {
+        if (ispunct(text[i]))
+        {
+            text.erase(i--, 1);
+            len = text.size();
+        }
+        else if (isdigit(text[i]))
+        {
+            text.erase(i--, 1);
+            len = text.size();
+        }
+    }
+    
+    //string to lower case
+    transform(text.begin(), text.end(), text.begin(), ::tolower);
+    
+    //putting all words into an array
+    int numWords = 0;
+    numWords = wordCount(text);
+    
+	istringstream ss(text);
+	int x = 0;
+	string words[numWords]= "";
+	int rating = 0, pos = 0 , neg = 0;
+	cout<<"SentiBOT: Currently checking word rating..."<<endl;
+	while (ss >> words[x]){
+		pos = checkPositive(conn, words[x]);
+		neg = checkNegative(conn, words[x]);
+		rating = rating + pos - neg;
+		x++;
+	}
+	return rating;
+}
 
 int main()
 {
@@ -112,7 +206,7 @@ int main()
 	{
 		cout<<"User: ";
 		getline(cin>>ws, userChat);
-		if(userChat=="exit"||userChat=="Exit"||userChat=="EXIT"){ //if user wants to quit
+		if(userChat=="exit"||userChat=="Exit"||userChat=="EXIT"){
 			cout<<"Are you sure?\n";
 			cout<< "[1]: Yes" << endl;
 			cout<< "[0]: No" << endl;
@@ -120,10 +214,21 @@ int main()
 			cin>>choice;
 			cin.ignore(100, '\n');
 				if (choice == "1" || choice =="Yes" || choice =="yes"){
-					cout<<"Thank you for using SentiBOT. Goodbye :D";
+					cout<<"Thank you for using SentiBOT. Goodbye :D"<<endl;
 					sessionCounter==0; //close program
 					break;	
 				}
+		}
+		else if (userChat == "review"){
+			cout<<"SentiBOT: Please input text to be analyzed."<< endl << "User: ";
+			getline(cin>>ws, userChat);
+			cin.ignore(100, '\n');
+			int rate;
+			cout<<"SentiBOT: I'm analyzing the statement, it might take a minute..."<<endl;
+			rate = cleanStatement(conn,userChat);
+			cout<<"SentiBOT: Thanks for leaving a review";
+			//increment respective column and product depending on the review if it was positive or negative
+	
 		}
 		else if(userChat=="!help" ||userChat=="!Help"||userChat=="!HELP"){
 			cout<<"Hi I am SentiBOT. A chatbot designed to assist you with your product reviews."<<endl;
@@ -131,19 +236,78 @@ int main()
 			cout<<"For reviewing our KIBU keyboard please type KIBU review or KIBU"<<endl<<endl;
 		}
 		else if (userChat=="RATT review" || userChat=="RATT"){
-			cout<<"What can you say about our product?"<<endl;
+			cout<<"What can you say about our RATT mouse?"<<endl;
 			getline(cin>>ws, userChat);
+			int rate;
+			cout<<"SentiBOT: I'm analyzing the statement, it might take a minute..."<<endl;
+			rate = cleanStatement(conn,userChat);
+			cout<<rate<<endl;//need tanggalin for feedback lang
+			if (rate>=1){
+				cout<<"SentiBOT: Positive Review"<<endl; //need tanggalin for feedback lang 
+				//INCREMENT POSITIVE REVIEW ON SUMMARY DATABASE
+			}
+			else if(rate<=-1){
+				cout<<"SentiBOT: Negative Review"<<endl;//need tanggalin for feedback lang
+				//INCREMENT NEGATIVE REVIEW ON SUMMARY DATABASE
+			}
+			else {
+				cout<<"SentiBOT: Neutral Review"<<endl;//need tanggalin for feedback lang
+				//INCREMENT NEUTRAL REVIEW ON SUMMARY DATABASE
+			}
+			//NEED ILAGAY SA TABLE YUNG RESULT NG REVIEW
+			cout<<"SentiBOT: Thanks for leaving a review on our RATT mouse. Would you like to keep chatting?(yes/no)"<<endl;
+			cout<<"User: ";
+			getline(cin>>ws,choice);
+			if (0 == strcasecmp(&choice[0],"yes")){
+				sessionCounter==1;
+				cout<<"SentiBOT: What's up? "<<endl;
+			}
+			else if (0 == strcasecmp(&choice[0],"no")){
+				sessionCounter==0;
+				cout<<"SentiBOT: Thanks for chatting with me! Hope to see you soon!"<<endl;
+				return 0;
+			}
+			
 		}
 		else if (userChat=="KIBU review" || userChat=="KIBU"){
-			cout<<"What can you say about our product?"<<endl;
+			cout<<"What can you say about our KIBU keyboard?"<<endl;
 			getline(cin>>ws, userChat);
+			int rate;
+			cout<<"SentiBOT: I'm analyzing the statement, it might take a minute..."<<endl;
+			rate = cleanStatement(conn,userChat);
+			cout<<rate<<endl;//need tanggalin for feedback lang
+			if (rate>=1){
+				cout<<"SentiBOT: Positive Review"<<endl;//need tanggalin for feedback lang
+				//INCREMENT POSITIVE REVIEW ON SUMMARY DATABASE
+			}
+			else if(rate<=-1){
+				cout<<"SentiBOT: Negative Review"<<endl;//need tanggalin for feedback lang
+				//INCREMENT NEGATIVE REVIEW ON SUMMARY DATABASE
+			}
+			else {
+				cout<<"SentiBOT: Neutral Review"<<endl;//need tanggalin for feedback lang
+				//INCREMENT NEUTRAL REVIEW ON SUMMARY DATABASE
+			}
+			//NEED ILAGAY SA TABLE YUNG RESULT NG REVIEW
+			cout<<"SentiBOT: Thanks for leaving a review on our KIBU keyboard. Would you like to keep chatting?(yes/no)"<<endl;
+			cout<<"User: ";
+			getline(cin>>ws,choice);
+			if (0 == strcasecmp(&choice[0],"yes")){
+				sessionCounter==1;
+				cout<<"SentiBOT: What's up? "<<endl;
+			}
+			else if (0 == strcasecmp(&choice[0],"no")){
+				sessionCounter==0;
+				cout<<"SentiBOT: Thanks for chatting with me! Hope to see you soon!"<<endl;
+				return 0;
+			}
 		}
-		else{ //normal conversation with user
-			display(conn,userChat,0);
+		else{
+			display(conn,userChat,0); //normal conversation with user
 		}
 		
 
 	}
-	
+	cout<<"SentiBOT: Thanks for chatting with me! Hope to see you soon!"<<endl;
 	return 0;
 }
