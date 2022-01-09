3d2387726022adfc671fe2479cff5664b4933125
AaronSy10 <90321660+AaronSy10@users.noreply.github.com>
2022-01-10 01:33:58 +0800

    Finalization

--- main.cpp
+++ main.cpp
@@ -306,7 +306,7 @@ int main()
 				}
 		}
 		else if(0 == strcasecmp(&userChat[0],"!help")){
-			cout<<"Peri: Hi I am SentiBOT. A chatbot designed to assist you with your product reviews."<<endl;
+			cout<<"Peri: Hi I am Pero. A chatbot designed to assist you with your peripherals."<<endl;
 			cout<<endl<<"-To review our RATT mouse, please type RATT review or RATT"<<endl;
 			cout<<"-To review our KIBU keyboard, please type KIBU review or KIBU"<<endl;
 			cout<<"-To add a response for Peri, please type the user input and provide a verification code to add to our conversation database."<<endl;
